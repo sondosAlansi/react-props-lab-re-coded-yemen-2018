438909b907900f135dd0ce03a8f546ca8138fad1
sondos alansi <sondos.alansi@yahoo.com>
2018-10-23 18:34:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
+
+const colorsArr=['blue','yellow'];
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship name="Millennium Falcon"
+  speed='150'
+  hasRockets=true
+  colors={colorsArr}
+  />,
   document.getElementById('global')
 );
